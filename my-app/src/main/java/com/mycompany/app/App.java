ba4d96610ffec930f4419025af57a6d4be58a22e
Teja251 <teja251@gmail.com>
2019-04-08 19:51:24 +0530

    Added 1

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Adding the sop statement for testing::::::::");
+        System.out.println("1");
     }
 }
